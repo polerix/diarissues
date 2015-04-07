4fbc0eca311a6904eaf9cafd3854eb00c609eb25
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-07 18:04:07 +0200

    render title test

--- js/main.js
+++ js/main.js
@@ -9,7 +9,8 @@ getAPI( "repos/" + username + "/" + reponame, renderTitle );
 getAPI( "repos/" + username + "/" + reponame + "/issues", renderIssues );
 function renderTitle(){
   var resp = JSON.parse(this.responseText);
-  console.log(resp);
+  document.querySelector('body > header > h1').innerHTML(resp['name']);
+  document.querySelector('body > header > p').innerHTML(resp['description']);
 }
 function renderIssues(){
   // ...however, the callback function is invoked AFTER the response arrives
